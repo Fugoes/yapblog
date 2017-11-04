6861f080a4830ee0835e37b2ae07fb6be634ca55
Fugoes <fugoes.qa@gmail.com>
2017-11-04 16:48:50 +0800

    fix typo

--- tests/test_api_user.py
+++ tests/test_api_user.py
@@ -3,9 +3,11 @@ import yapb.config as config
 
 url = "http://%s:%d/api/user" % (config.HOST, config.PORT)
 
-r = requests.post(url, data={"name": "user1", "email": "user1@example.com", "passwd": "slkdfjalsdf"})
-print(r.content.decode())
-r = requests.post(url, data={"name": "user2", "email": "user2@example.com", "passwd": "slkdxfjalsdf"})
-print(r.content.decode())
 r = requests.post(url, data={"name": "test", "email": "test@example.com", "passwd": "test"})
 print(r.content.decode())
+for i in range(1000):
+    r = requests.post(url, data={
+        "name": "test%d" % i,
+        "email": "test%d@example.com" % i,
+        "passwd": "test"
+    })
