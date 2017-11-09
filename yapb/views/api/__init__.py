befa9c33b7564a05924a8d0066da86b21ba72146
sunchaothu <sunchaothu@MechREV.localdomain>
2017-11-10 01:51:57 +0800

    add register

--- yapb/views/api/__init__.py
+++ yapb/views/api/__init__.py
@@ -1,3 +1,4 @@
 from yapb.views.api.user import *
 from yapb.views.api.login import *
 from yapb.views.api.logout import *
+from yapb.views.api.register import *
---
+++ yapb/views/api/register.py
@@ -0,0 +1,49 @@
+__all__=["api_register"]
+
+from flask import request
+from yapb import app
+from yapb.lib.api import ok,not_ok
+
+@app.route("/api/register",methods=["POST"])
+def api_register():
+    """
+    Register
+    Methof:POST
+    Parameters:name email passwd confirm passwd
+    Erro:{
+            "ok": False,
+	    "message","Entered password differ!"
+          }
+    Sucess:{
+          "ok" : True
+	   "msg":"Register scucessfully!"
+
+    }
+    """
+    try:
+        name = request.form["name"]
+    except KeyError:
+        return not_ok("no name")
+   
+    try:
+       email = request.form["email"]
+    except KeyError:
+         return not_ok["no email"]
+    
+    try:
+        passwd =  request.form["passwd"]
+    except KeyError:
+         return not_ok["no passwd"]
+    
+    try:
+        confirm_passwd =  request.form["confirm_passwd"]
+    except KeyError:
+         return not_ok["no confirm_passwd"]
+    
+
+    if passwd != confirm_passwd:
+       return not_ok["Entered password differ!"]
+    else:
+       # TODO
+       return ok["Register sucessfully!"]
+
