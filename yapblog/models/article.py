2f27f9851b8e66e424ed61c72f78b5724c9b5097
Fugoes <fugoes.qa@gmail.com>
2017-11-17 21:42:57 +0800

    add api/article

--- yapblog/models/article.py
+++ yapblog/models/article.py
@@ -1,46 +1,65 @@
+__all__ = ["Article", "Page", "Comment"]
+
+from sqlalchemy.exc import IntegrityError
 from yapblog import db
 
 
 class Article(db.Model):
+    __tablename__ = "article"
     # Primary key
-    _id = db.Column("id", db.Integer, primary_key=True)
-    # Foreign keys
-    _author_id = db.Column(
-        "author_id", db.Integer, db.ForeignKey("user.id"),
-        nullable=False)
-    _html_content_id = db.Column(
-        "html_content_id", db.Integer, db.ForeignKey("content.id"),
-        nullable=False)
-    _page_id = db.Column(
+    id_ = db.Column("id", db.Integer, primary_key=True)
+    # Properties
+    title_ = db.Column("title", db.String(1024), nullable=False, unique=True)
+    date_ = db.Column("date", db.Date, nullable=False)
+    html_content_ = db.Column("html_content", db.Text, nullable=False)
+    # One to One
+    page_id_ = db.Column(
         "page_id", db.Integer, db.ForeignKey("page.id"),
         nullable=False)
-    # Properties
-    _date = db.Column("date", db.DateTime, nullable=False)
-    _title = db.Column("title", db.String(1024), nullable=False)
 
+    def __init__(self, title, date, html_content, page_id):
+        self.title_ = title
+        self.date_ = date
+        self.html_content_ = html_content
+        self.page_id_ = page_id
 
-class Content(db.Model):
-    # Primary key
-    _id = db.Column("id", db.Integer, primary_key=True)
-    # Properties
-    _value = db.Column("value", db.Text, nullable=False)
-
-
-class ArticleTag(db.Model):
-    _article_id = db.Column("article_id", db.ForeignKey("article.id"), db.Integer, nullable=False)
-    _tag_id = db.Column("tag_id", db.ForeignKey("tag.id"), db.Integer, nullable=False)
+    @staticmethod
+    def new(title, date, html_content):
+        new_page = Page.new()
+        new_article = Article(title, date, html_content, new_page.id_)
+        db.session.add(new_article)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            return None
+        return new_article
 
 
 class Page(db.Model):
-    # Primary Key
-    _id = db.Column("id", db.Integer, primary_key=True)
+    __tablename__ = "page"
+    # Primary key
+    id_ = db.Column("id", db.Integer, primary_key=True)
+    comments_ = db.relationship("Comment",
+                                backref="page", lazy=True)
+
+    def __init__(self):
+        return
+
+    @staticmethod
+    def new():
+        new_page = Page()
+        db.session.add(new_page)
+        db.session.commit()
+        return new_page
 
 
 class Comment(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    _value = db.Column("value", db.Text, nullable=False)
-
-
-class CommentReply(db.Model):
-    _id = db.Column("id", db.ForeignKey("comment.id"), db.Integer, nullable=False, unique=True)
-    _reply_to_id = db.Column("reply_to_id", db.ForeignKey("comment.id"), db.Integer, nullable=False)
+    __tablename__ = "comment"
+    # Primary key
+    id_ = db.Column("id", db.Integer, primary_key=True)
+    # Properties
+    text_ = db.Column("text", db.Text, nullable=False)
+    # One to One
+    reply_to_id_ = db.Column("reply_to_id", db.ForeignKey("comment.id"), nullable=True)
+    # Many to One
+    page_id_ = db.Column("page_id", db.Integer, db.ForeignKey("page.id"), nullable=True)
