8cc39856e10b2887fc847549e255aa7216f5cc1d
Fugoes <fugoes.qa@gmail.com>
2017-11-17 22:40:29 +0800

    update

--- yapblog/lib/page.py
+++ yapblog/lib/page.py
@@ -1,20 +1,10 @@
-class Page(object):
-    class NavBar(object):
-        class Item(object):
-            def __init__(self, **kwargs):
-                self.is_active = kwargs["is_active"]
-                self.link = kwargs["link"]
-                self.text = kwargs["text"]
-
+class NavBar(object):
+    class Item(object):
         def __init__(self, **kwargs):
-            self.title = kwargs["title"]
-            self.items = kwargs["items"]
+            self.is_active = kwargs["is_active"]
+            self.link = kwargs["link"]
+            self.text = kwargs["text"]
 
-    class SideBar(object):
-        pass
-
-    class Content(object):
-        pass
-
-    def __init__(self):
-        self.navbar = Page.NavBar()
+    def __init__(self, **kwargs):
+        self.title = kwargs["title"]
+        self.items = kwargs["items"]
