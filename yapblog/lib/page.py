6dc720b33adefb3dded593e31eaba8d0a433465f
Fugoes <fugoes.qa@gmail.com>
2017-11-18 10:35:32 +0800

    add basic admin for articles

--- yapblog/lib/page.py
+++ yapblog/lib/page.py
@@ -8,3 +8,20 @@ class NavBar(object):
     def __init__(self, **kwargs):
         self.title = kwargs["title"]
         self.items = kwargs["items"]
+
+
+class SideBar(object):
+    class CollapsibleList(object):
+        class Item(object):
+            def __init__(self, **kwargs):
+                self.link = kwargs["link"]
+                self.text = kwargs["text"]
+
+        def __init__(self, **kwargs):
+            self.id = kwargs["id"]
+            self.title = kwargs["title"]
+            self.items = kwargs["items"]
+            self.template = "element/collapsible_list.html"
+
+    def __init__(self, **kwargs):
+        self.items = kwargs["items"]
